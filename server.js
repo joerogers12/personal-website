2946d7d50b1718baa99c6576278b0d904157e8c3
Joe Rogers <frogers@uci.edu>
2025-07-19 17:09:30 -0700

    Implemented js server

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 import express from "express";
-import axios from "axios";
+import js from "js";
+import path from "path";
+import dotenv from "dotenv";
+import fetchTopArtists from "./spotify";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get()
+//
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Periodically refresh top artists (every hour)
+const REFRESH_RATE = 1000  * 60 * 60;
+setInterval(fetchTopArtists, REFRESH_RATE);
+
+// Fetch artists immediately upon startup
+fetchTopArtists();
+
+// API endpoint to serve cached data
+app.get("/api/top-artists", (req, res) => {
+  const artists = JSON.parse(fs.readFileSync("./topArtists.json", "utf-8"));
+  res.json(artists);
+});
+
+// Homepage with top artists
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${port}`));
